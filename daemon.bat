79fc321dff188c7c01ebed7fe0e6afa3270f401e
verkruemelt <verkruemelt@gmail.com>
2013-05-18 19:51:23 +0200

    repo reorganized
    bin/ for executed files
    hash/ for generated has files
    added parameters
    added different hash algoritm (crc32 standard)
    modified script for new structure
    
    Signed-off-by: verkruemelt <verkruemelt@gmail.com>

--- daemon.bat
+++ daemon.bat
@@ -1,35 +1,40 @@
 @ECHO OFF
-TITLE SHA seventeen Zip
-SET source="C:\Windows"
-SET backup="backup"
+SET source="C:\Users\"
+SET backup="D:\backup\profiles"
 SET prefix=""
 SET suffix=""
 SET seconds=300
-SET fastest=-mx1 -md=64k -mfb=32 -ms=8m
-SET fast=-mx3 -md=1m -mfb=32 -ms=128m
-SET normal=-mx5 -md=16m -mfb=32 -ms=2g
-SET maximum=-mx7 -md=32m -mfb=64 -ms=4g
-SET ultra=-mx9 -md=64m -mfb=64 -ms=4g
+SET "fastest=-mx1 -md=64k -mfb=32 -ms=8m"
+SET "fast=-mx3 -md=1m -mfb=32 -ms=128m"
+SET "normal=-mx5 -md=16m -mfb=32 -ms=2g"
+SET "maximum=-mx7 -md=32m -mfb=64 -ms=4g"
+SET "ultra=-mx9 -md=64m -mfb=64 -ms=4g"
+SET alg=crc32
+SET index=hash\index.%alg%
+SET check=hash\check.%alg%
 SET compress=%fastest%
-IF %processor_architecture% == x86 (SET sha1=sha1sum.exe)
-IF %processor_architecture% == AMD64 (SET sha1=sha1sum64.exe)
+IF %processor_architecture% == x86 (SET hash=bin\%alg%sum.exe)
+IF %processor_architecture% == AMD64 (SET hash=bin\%alg%sum64.exe)
+TITLE hash7z - %source% ^> %backup%
 :loop
 CLS
-IF NOT EXIST INDEX (
+IF NOT EXIST %index% (
 	ECHO create index..
-	%sha1% -r %source%\* > INDEX
+	%hash% -r %source%\* > %index%
 	)
 CLS
 ECHO check index..
-%sha1% -r %source%\* > CHECK
+%hash% -r %source%\* > %check%
 CLS
 FOR /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 SET datetime=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%_%ldt:~8,2%%ldt:~10,2%%ldt:~12,2%
-ECHO Last check @ %datetime%
-FC INDEX CHECK > NUL
-IF %errorlevel% NEQ 0 (
-	MOVE CHECK INDEX > NUL
-	7za.exe a -t7z -m0=LZMA -mmt=on %compress% -sccUTF-8 -w%backup% %backup%\%prefix%%datetime%%suffix%.7z %source%
+ECHO last check @ %datetime%
+FC %index% %check% > NUL
+IF %errorlevel% EQU 0 (
+	DEL %check%
+	) ELSE (
+	MOVE %check% %index% > NUL
+	bin\7za.exe a -t7z -m0=LZMA -mmt=on %compress% -sccUTF-8 -w%backup% %backup%\%prefix%%datetime%%suffix%.7z %source%
 	ECHO backup created @ %datetime%
 	)
 timeout /T %seconds% > NUL
