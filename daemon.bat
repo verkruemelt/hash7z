cf0dd3cc703ff356d54cfc3545c6a66fe84fc486
verkruemelt <verkruemelt@gmail.com>
2013-05-18 13:30:40 +0200

    several improvements
    parameterized compression
    environment independent date time function
    reworked algorithm
    messages on command line

--- daemon.bat
+++ daemon.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-
+TITLE SHA seventeen Zip
 SET source="C:\Windows"
 SET backup="backup"
 SET prefix=""
@@ -10,19 +10,27 @@ SET fast=-mx3 -md=1m -mfb=32 -ms=128m
 SET normal=-mx5 -md=16m -mfb=32 -ms=2g
 SET maximum=-mx7 -md=32m -mfb=64 -ms=4g
 SET ultra=-mx9 -md=64m -mfb=64 -ms=4g
+SET compress=%fastest%
 IF %processor_architecture% == x86 (SET sha1=sha1sum.exe)
 IF %processor_architecture% == AMD64 (SET sha1=sha1sum64.exe)
-
 :loop
-IF NOT EXIST HASH (%sha1% -r %source%\* > HASH)
+CLS
+IF NOT EXIST INDEX (
+	ECHO create index..
+	%sha1% -r %source%\* > INDEX
+	)
+CLS
+ECHO check index..
 %sha1% -r %source%\* > CHECK
-FC HASH CHECK
-IF %errorlevel% == 1 (
-	MOVE CHECK HASH
-	7za.exe a -t7z -m0=LZMA -mmt=on %fastest% -sccUTF-8 -w%backup% %backup%\%prefix%%date:~6,4%-%date:~3,2%-%date:~0,2%" "%time:~0,2%-%time:~3,2%-%time:~6,2%%suffix%.7z %source%
-	echo backup created @ %date:~6,4%-%date:~3,2%-%date:~0,2% %time:~0,2%-%time:~3,2%-%time:~6,2%
-	
+CLS
+FOR /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+SET datetime=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%_%ldt:~8,2%%ldt:~10,2%%ldt:~12,2%
+ECHO Last check @ %datetime%
+FC INDEX CHECK > NUL
+IF %errorlevel% NEQ 0 (
+	MOVE CHECK INDEX > NUL
+	7za.exe a -t7z -m0=LZMA -mmt=on %compress% -sccUTF-8 -w%backup% %backup%\%prefix%%datetime%%suffix%.7z %source%
+	ECHO backup created @ %datetime%
 	)
 timeout /T %seconds% > NUL
-CLS
 GOTO loop
